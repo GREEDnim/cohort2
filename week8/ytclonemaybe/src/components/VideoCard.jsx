4bee0c8e0b2e487e0dd6883482a21c9d0db30393
Sivadhas <sivadhas007@gmail.com>
2024-01-31 09:28:07 +0530

    yt clone

--- week8/ytclonemaybe/src/components/VideoCard.jsx
+++ week8/ytclonemaybe/src/components/VideoCard.jsx
@@ -1,21 +1,6 @@
 function VideoCard(){
     return(
-        <div>
-            <img src="zoro.jpg" className="w-[500px] rounded-xl" alt=""  />
-            <div className="grid grid-cols-12 pt-2">
-                <div className="col-span-1">
-                    <img src="logo.jpg" className="rounded-full w-18 h-18" alt=""  />
-                </div>
-                
-                <div className="col-span-11 ">
-                    <div className="text-white" >Zoro is the GOAT</div>
-                    <div className="text-gray-400 text-base	">OnePiece</div>
-                    <div className="text-gray-400 text-base	">69M . 69 days ago</div>
-                </div>
-            </div>
-        </div>
-        
-        
+    
     )
 }
 export default VideoCard;
