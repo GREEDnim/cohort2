cab6f544eeae961b630a1b4135648573dcf6d865
Sivadhas <sivadhas007@gmail.com>
2024-01-31 09:08:32 +0530

    paytm-be

---
+++ week8/ytclonemaybe/src/App.jsx
@@ -0,0 +1,8 @@
+import VideoCard from "./components/VideoCard"
+export default function App() {
+  return (
+    <div className=" h-screen bg-black">
+      <VideoCard></VideoCard>
+    </div>
+  )
+}
